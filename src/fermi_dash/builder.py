009b8bef256d61aa0e541f75cc447427159640f4
cameronrulten <cbrulten@umn.edu>
2025-09-11 09:15:05 +0100

    Make tabs dynamic

--- src/fermi_dash/builder.py
+++ src/fermi_dash/builder.py
@@ -242,7 +242,7 @@ def build_dashboard(opts: BuildOptions) -> Path:
         sizing_mode="stretch_width",
     )
 
-    tabs_obj = pn.Tabs(*tabs, dynamic=False)
+    tabs_obj = pn.Tabs(*tabs, dynamic=True)
 
     template = pn.template.FastListTemplate(
         title=opts.title,
